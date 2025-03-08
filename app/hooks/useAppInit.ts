0777ddcebef9e07bff9b205a23a4993d9ba3fcad
Pankaj Kumar <pankaj@richpanel.com>
2025-03-08 23:33:33 +0530

    fetch categories on app startup

--- app/hooks/useAppInit.ts
+++ app/hooks/useAppInit.ts
@@ -1,5 +1,5 @@
 import { useAppDispatch } from "@/store/store"
-import { fetchTransactions } from "@/store/transactions/transactionsSlice"
+import { fetchCategories, fetchTransactions } from "@/store/transactions/transactionsSlice"
 import { useEffect } from "react"
 
 export const useAppInit = () => {
@@ -7,5 +7,6 @@ export const useAppInit = () => {
   // Fetch transactions and categories on component mount
   useEffect(() => {
     dispatch(fetchTransactions())
+    dispatch(fetchCategories())
   }, [dispatch])
 }
