d8a18a1dfc6ebf93fe62d5d35b508fc11a0e404e
Pankaj Kumar <pankaj@richpanel.com>
2025-03-09 01:50:01 +0530

    clean

--- app/components/Dashboard/BudgetOverview.tsx
+++ app/components/Dashboard/BudgetOverview.tsx
@@ -9,10 +9,8 @@ import { CardHeader, ContentCard } from "@/components/AdvancedCard"
 import { LinkButton } from "../Buttons"
 import { useAppSelector } from "@/store/store"
 import { BudgetList } from "../Budget/BudgetList"
-import { ThemedStyle } from "@/theme"
 
 export const BudgetOverview = () => {
-  const { themed } = useAppTheme()
   const navigation = useNavigation()
 
   const { budgets } = useAppSelector((state) => state.budget)
@@ -30,23 +28,3 @@ export const BudgetOverview = () => {
     </ContentCard>
   )
 }
-
-const $emptyContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
-  flex: 1,
-  justifyContent: "center",
-  alignItems: "center",
-  paddingHorizontal: spacing.lg,
-})
-
-const $emptyText: ThemedStyle<TextStyle> = ({ spacing }) => ({
-  fontSize: 18,
-  fontWeight: "600",
-  textAlign: "center",
-  marginBottom: spacing.sm,
-})
-
-const $emptySubText: ThemedStyle<TextStyle> = ({ colors }) => ({
-  fontSize: 16,
-  color: colors.textDim,
-  textAlign: "center",
-})
