f8af17579d2d540389aa9ef932ce160276536b49
Pankaj Kumar <pankaj@richpanel.com>
2025-03-09 12:50:09 +0530

    app startup required app calls

--- app/hooks/useAppInit.ts
+++ app/hooks/useAppInit.ts
@@ -1,12 +1,17 @@
+import { fetchBudgetsByMonth } from "@/store/budget/budgetSlice"
+import { fetchDashboardData } from "@/store/dashboard/dashboardSlice"
 import { useAppDispatch } from "@/store/store"
 import { fetchCategories, fetchTransactions } from "@/store/transactions/transactionsSlice"
+import { getCurrentMonth } from "@/utils/formatDate"
 import { useEffect } from "react"
 
 export const useAppInit = () => {
   const dispatch = useAppDispatch()
   // Fetch transactions and categories on component mount
   useEffect(() => {
+    dispatch(fetchDashboardData())
     dispatch(fetchTransactions())
     dispatch(fetchCategories())
+    dispatch(fetchBudgetsByMonth(getCurrentMonth()))
   }, [dispatch])
 }
